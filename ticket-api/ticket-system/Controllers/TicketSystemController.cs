e6c811a7b31a5b55aa30bc3c67f2e0024cef9aa5
Aleena Maryam <aleenamaryam@gmail.com>
2026-03-28 19:44:18 -0400

    net boilerplate

---
+++ ticket-api/ticket-system/Controllers/TicketSystemController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ticket_system.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketSystemController : ControllerBase
+    {
+       
+    }
+}
