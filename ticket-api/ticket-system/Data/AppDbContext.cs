30c302fba8639b0e05a32434c2292e1cdfd468fb
Aleena Maryam <aleenamaryam@gmail.com>
2026-04-27 19:54:14 -0400

    update EF Core relationships

--- ticket-api/ticket-system/Data/AppDbContext.cs
+++ ticket-api/ticket-system/Data/AppDbContext.cs
@@ -10,29 +10,56 @@ namespace ticket_system.Data
 
         // EF Core Tables
         public DbSet<User> Users { get; set; }
+        
         public DbSet<Ticket> Tickets { get; set; }
+
         public DbSet<Comment> Comments { get; set; }
+
         public DbSet<Board> Boards { get; set; }
+
+        public DbSet<Column> Columns { get; set; }
 
         // have to prevent cascading deletes
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder
+            _ = modelBuilder
                 .Entity<Ticket>()
                 .HasOne(t => t.Creator)
                 .WithMany() // can put something here if collection added later
                 .HasForeignKey(t => t.CreatorId)
-                .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.NoAction);
+                .OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder
+            _ = modelBuilder
                 .Entity<Ticket>()
                 .HasOne(t => t.Assignee)
                 .WithMany()
                 .HasForeignKey(t => t.AssigneeId)
-                .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.NoAction);
+                .OnDelete(DeleteBehavior.NoAction);
 
-            // for scaffolding
-            modelBuilder.Entity<User>().HasData(new User { Id = 1, Name = "Admin" });
+            _ = modelBuilder
+                .Entity<Column>()
+                .HasOne(c => c.Board)
+                .WithMany(b => b.Columns)
+                .HasForeignKey(c => c.BoardId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            _ = modelBuilder
+                .Entity<Comment>()
+                .HasOne(c => c.Ticket)
+                .WithMany(t => t.Comments)
+                .HasForeignKey(c => c.TicketId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            _ = modelBuilder
+                .Entity<Ticket>()
+                .HasOne(t => t.Column)
+                .WithMany(c => c.Tickets)
+                .HasForeignKey(t => t.ColumnId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+             // should column delete cascade or be restricted
+
+            _ = modelBuilder.Entity<User>().HasData(new User { Id = 1, Name = "Admin" });
         }
     }
 }
